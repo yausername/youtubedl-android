1f000d50eb9c47a43573e7ddd25be86154530ecd
Ritvik Saraf <13ritvik@gmail.com>
2019-02-16 15:47:50 +0530

    added manifest url

--- library/src/main/java/com/yausername/youtubedl_android/mapper/VideoInfo.java
+++ library/src/main/java/com/yausername/youtubedl_android/mapper/VideoInfo.java
@@ -56,6 +56,8 @@ public class VideoInfo {
     public ArrayList<VideoFormat> formats;
     public ArrayList<VideoThumbnail> thumbnails;
     //public ArrayList<VideoSubtitle> subtitles;
+	@JsonProperty("manifest_url")
+	public String manifestUrl;
     
     //some useful getters
 	public String getViewCount() {
