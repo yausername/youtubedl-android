9c542eed32ee4396f83520723f0b09d9c04b0cd0
yausername <yauser@protonmail.com>
2020-07-31 02:55:22 +0530

    updated info class

--- library/src/main/java/com/yausername/youtubedl_android/mapper/VideoThumbnail.java
+++ library/src/main/java/com/yausername/youtubedl_android/mapper/VideoThumbnail.java
@@ -4,6 +4,14 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class VideoThumbnail {
-    public String url;
-    public String id;
+    private String url;
+    private String id;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
