b2b5752bce9ac5f09ac4faa71d0fb8cef798be82
p3g4asus <3275734+p3g4asus@users.noreply.github.com>
2022-08-09 07:58:31 +0200

    Example updated Readme example added

--- app/src/main/java/com/yausername/youtubedl_android_example/CommandExampleActivity.java
+++ app/src/main/java/com/yausername/youtubedl_android_example/CommandExampleActivity.java
@@ -34,6 +34,7 @@ import io.reactivex.schedulers.Schedulers;
 public class CommandExampleActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnRunCommand;
+    private Button btnStopDownload;
     private EditText etCommand;
     private ProgressBar progressBar;
     private TextView tvCommandStatus;
@@ -42,6 +43,7 @@ public class CommandExampleActivity extends AppCompatActivity implements View.On
 
     private boolean running = false;
     private final CompositeDisposable compositeDisposable = new CompositeDisposable();
+    private final String processId = "MyMainDownload";
 
     private final DownloadProgressCallback callback = new DownloadProgressCallback() {
         @Override
@@ -67,6 +69,7 @@ public class CommandExampleActivity extends AppCompatActivity implements View.On
 
     private void initViews() {
         btnRunCommand = findViewById(R.id.btn_run_command);
+        btnStopDownload = findViewById(R.id.btn_stop_download);
         etCommand = findViewById(R.id.et_command);
         progressBar = findViewById(R.id.progress_bar);
         tvCommandStatus = findViewById(R.id.tv_status);
@@ -76,12 +79,25 @@ public class CommandExampleActivity extends AppCompatActivity implements View.On
 
     private void initListeners() {
         btnRunCommand.setOnClickListener(this);
+        btnStopDownload.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btn_run_command) {
-            runCommand();
+        switch (v.getId()) {
+            case R.id.btn_run_command:
+                runCommand();
+                break;
+            case R.id.btn_stop_download:
+                if (running) {
+                    try {
+                        YoutubeDL.getInstance().destroyProcessById(processId);
+                        running = false;
+                    } catch (Exception e) {
+                        Log.e(TAG, e.toString());
+                    }
+                }
+                break;
         }
     }
 
@@ -118,7 +134,7 @@ public class CommandExampleActivity extends AppCompatActivity implements View.On
         showStart();
 
         running = true;
-        Disposable disposable = Observable.fromCallable(() -> YoutubeDL.getInstance().execute(request, callback))
+        Disposable disposable = Observable.fromCallable(() -> YoutubeDL.getInstance().execute(request, callback, processId))
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(youtubeDLResponse -> {
