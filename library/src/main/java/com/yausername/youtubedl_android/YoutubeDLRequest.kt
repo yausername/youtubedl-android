aca1f36706f968a1d4dab7268d6ae16b062b673a
junkfood <69683722+JunkFood02@users.noreply.github.com>
2022-12-23 22:19:07 +0800

    Fix Java & Kotlin interoperability

--- library/src/main/java/com/yausername/youtubedl_android/YoutubeDLRequest.kt
+++ library/src/main/java/com/yausername/youtubedl_android/YoutubeDLRequest.kt
@@ -1,15 +1,15 @@
 package com.yausername.youtubedl_android
 
 class YoutubeDLRequest {
-    private val urls: List<String?>
+    private val urls: List<String>
     private val options = YoutubeDLOptions()
-    private val customCommandList: MutableList<String?> = ArrayList()
+    private val customCommandList: MutableList<String> = ArrayList()
 
-    constructor(url: String?) {
+    constructor(url: String) {
         urls = listOf(url)
     }
 
-    constructor(urls: List<String?>) {
+    constructor(urls: List<String>) {
         this.urls = urls
     }
 
@@ -28,8 +28,8 @@ class YoutubeDLRequest {
         return this
     }
 
-    fun addCommands(commands: List<String?>?): YoutubeDLRequest {
-        customCommandList.addAll(commands!!)
+    fun addCommands(commands: List<String>): YoutubeDLRequest {
+        customCommandList.addAll(commands)
         return this
     }
 
@@ -45,8 +45,8 @@ class YoutubeDLRequest {
         return options.hasOption(option)
     }
 
-    fun buildCommand(): List<String?> {
-        val commandList: MutableList<String?> = ArrayList()
+    fun buildCommand(): List<String> {
+        val commandList: MutableList<String> = ArrayList()
         commandList.addAll(options.buildOptions())
         commandList.addAll(customCommandList)
         commandList.addAll(urls)
