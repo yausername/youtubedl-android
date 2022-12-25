2b14a1c6d1c7e057b709575d9d1892a349708372
junkfood <69683722+JunkFood02@users.noreply.github.com>
2022-12-26 02:08:10 +0800

    Revert changes

--- library/src/main/java/com/yausername/youtubedl_android/YoutubeDLException.kt
+++
@@ -1,7 +0,0 @@
-package com.yausername.youtubedl_android
-
-class YoutubeDLException : Exception {
-    constructor(message: String?) : super(message) {}
-    constructor(message: String?, e: Throwable?) : super(message, e) {}
-    constructor(e: Throwable?) : super(e) {}
-}
