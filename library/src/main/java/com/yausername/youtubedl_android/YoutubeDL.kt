a7009b52631c44c58dbade861ddd966852e332e3
Gabriel Fontán <gabilessto@gmail.com>
2023-11-19 00:10:29 +0100

    Kind of merged the last commit of the repo; added MASTER downloadable yt-dlp

--- library/src/main/java/com/yausername/youtubedl_android/YoutubeDL.kt
+++ library/src/main/java/com/yausername/youtubedl_android/YoutubeDL.kt
@@ -239,6 +239,8 @@ object YoutubeDL {
         object STABLE : UpdateChannel("https://api.github.com/repos/yt-dlp/yt-dlp/releases/latest")
         object NIGHTLY :
             UpdateChannel("https://api.github.com/repos/yt-dlp/yt-dlp-nightly-builds/releases/latest")
+        object MASTER :
+            UpdateChannel("https://api.github.com/repos/yt-dlp/yt-dlp-master-builds/releases/latest")
 
         companion object {
             @JvmField
@@ -246,6 +248,9 @@ object YoutubeDL {
 
             @JvmField
             val _NIGHTLY: NIGHTLY = NIGHTLY
+
+            @JvmField
+            val _MASTER: MASTER = MASTER
         }
     }
 
