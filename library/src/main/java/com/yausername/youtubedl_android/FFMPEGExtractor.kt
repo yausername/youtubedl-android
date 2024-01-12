1ac48c0068ec89b0f1a9f29511c971ee22be1dc1
Farman Ullah Khan <farimarwat@gmail.com>
2024-01-12 14:41:56 +0500

    Crashes fixed

--- library/src/main/java/com/yausername/youtubedl_android/FFMPEGExtractor.kt
+++ library/src/main/java/com/yausername/youtubedl_android/FFMPEGExtractor.kt
@@ -28,31 +28,35 @@ class FFMPEGExtractor{
     ):Thread(){
         var shouldContinue = true
         override fun run() {
-            val pythonPID = ProcessUtils.getPythonProcessId(process)
-            var ffmpegstarted = false
-            var line: String? = null
-            while(shouldContinue){
-                val ffmpegPid = ProcessUtils.getFFMPEGProcessId(pythonPID)
-                val progressFilePath = "/proc/$ffmpegPid/fd/2"
-                val progressfile = File(progressFilePath)
-                if(ffmpegPid > 0){
-                    ffmpegstarted = true
-                }
-                if (progressfile.exists()) {
-                    val inputStream = FileInputStream(progressfile)
-                    val reader = BufferedReader(InputStreamReader(inputStream))
-                    while (reader.readLine().also { line = it } != null) {
-                        val size = ProcessUtils.extractSize(line)
-                        progressCallback?.let { it(size,line,ffmpegstarted) }
-                    }
-                }else {
-                    if(ffmpegstarted && ffmpegPid < 0){
-                        progressCallback?.let { it(-1,line,false) }
-                    }
-                }
+           try{
+               val pythonPID = ProcessUtils.getPythonProcessId(process)
+               var ffmpegstarted = false
+               var line: String? = null
+               while(shouldContinue){
+                   val ffmpegPid = ProcessUtils.getFFMPEGProcessId(pythonPID)
+                   val progressFilePath = "/proc/$ffmpegPid/fd/2"
+                   val progressfile = File(progressFilePath)
+                   if(ffmpegPid > 0){
+                       ffmpegstarted = true
+                   }
+                   if (progressfile.exists()) {
+                       val inputStream = FileInputStream(progressfile)
+                       val reader = BufferedReader(InputStreamReader(inputStream))
+                       while (reader.readLine().also { line = it } != null) {
+                           val size = ProcessUtils.extractSize(line)
+                           progressCallback?.let { it(size,line,ffmpegstarted) }
+                       }
+                   }else {
+                       if(ffmpegstarted && ffmpegPid < 0){
+                           progressCallback?.let { it(-1,line,false) }
+                       }
+                   }
 
-                sleep(100)
-            }
+                   sleep(100)
+               }
+           }catch (ex:Exception){
+               throw IOException()
+           }
         }
 
         fun stopNow(){
